a56d78d61b0124e11b0f8ab320ef6568b203c285
Greg Duckworth <gregory.duckworth@bbc.co.uk>
2016-10-11 14:09:19 +0100

    Removed register email

--- resources/views/emails/users/new.blade.php
+++
@@ -1,6 +0,0 @@
-Hi {!! $user->name !!}
-
-Before you are able to access the system, please request a password reset here:
-<a href="{!! route('password.reset') !!}">{!! route('password.reset') !!}</a>
-
-Thanks,
