45d14caf2ff4b586a5e1df02f8dbf00f8967828a
Greg Duckworth <gregory.duckworth@bbc.co.uk>
2016-10-18 15:16:06 +0100

    Update error pages

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,29 +1,22 @@
-@extends('layouts.app')
+@extends(Auth::check() ? 'layouts.app' : 'layouts.auth')
 
-@section('htmlheader_title')
-    {{ trans('adminlte_lang::message.forbidden') }}
-@endsection
+@section('htmlheader_title', trans('message.accessdenied'))
 
-@section('contentheader_title')
-    {{ trans('adminlte_lang::message.403error') }}
-@endsection
-
-@section('$contentheader_description')
-@endsection
+@section('contentheader_title', trans('message.403error'))
 
 @section('main-content')
-
+<body>
 <div class="error-page">
-    <h2 class="headline text-yellow"> 403</h2>
+    <h2 class="headline text-red">403</h2>
     <div class="error-content">
-        <h3><i class="fa fa-warning text-yellow"></i> Oops! {{ trans('adminlte_lang::message.forbidden') }}.</h3>
+        <h3><i class="fa fa-warning text-red"></i> Oops! {!! trans('message.accessdenied') !!}.</h3>
         <p>
-            {{ trans('adminlte_lang::message.forbidden') }}
-            {{ trans('adminlte_lang::message.mainwhile') }} <a href='{{ url('/home') }}'>{{ trans('adminlte_lang::message.returndashboard') }}</a> {{ trans('adminlte_lang::message.usingsearch') }}
+            {!! trans('message.notfindpage') !!}
+            {!! trans('message.mainwhile') !!} <a href='{!! url('/home') !!}'>{!! trans('message.returndashboard') !!}</a> {!! trans('message.usingsearch') !!}
         </p>
         <form class='search-form'>
             <div class='input-group'>
-                <input type="text" name="search" class='form-control' placeholder="{{ trans('adminlte_lang::message.search') }}"/>
+                <input type="text" name="search" class='form-control' placeholder="{!! trans('message.search') !!}"/>
                 <div class="input-group-btn">
                     <button type="submit" name="submit" class="btn btn-warning btn-flat"><i class="fa fa-search"></i></button>
                 </div>
@@ -31,4 +24,5 @@
         </form>
     </div><!-- /.error-content -->
 </div><!-- /.error-page -->
+</body>
 @endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,29 +1,22 @@
-@extends('layouts.app')
+@extends(Auth::check() ? 'layouts.app' : 'layouts.auth')
 
-@section('htmlheader_title')
-    {{ trans('adminlte_lang::message.servererror') }}
-@endsection
+@section('htmlheader_title', trans('message.servererror'))
 
-@section('contentheader_title')
-    {{ trans('adminlte_lang::message.500error') }}
-@endsection
-
-@section('$contentheader_description')
-@endsection
+@section('contentheader_title', trans('message.500error'))
 
 @section('main-content')
-
+<body>
     <div class="error-page">
         <h2 class="headline text-red">500</h2>
         <div class="error-content">
-            <h3><i class="fa fa-warning text-red"></i> Oops! {{ trans('adminlte_lang::message.somethingwrong') }}</h3>
+            <h3><i class="fa fa-warning text-red"></i> Oops! {!! trans('message.somethingwrong') !!}</h3>
             <p>
-                {{ trans('adminlte_lang::message.wewillwork') }}
-                {{ trans('adminlte_lang::message.mainwhile') }} <a href='{{ url('/home') }}'>{{ trans('adminlte_lang::message.returndashboard') }}</a> {{ trans('adminlte_lang::message.usingsearch') }}
+                {!! trans('message.wewillwork') !!}
+                {!! trans('message.mainwhile') !!} <a href='{!! url('/home') !!}'>{!! trans('message.returndashboard') !!}</a> {!! trans('message.usingsearch') !!}
             </p>
             <form class='search-form'>
                 <div class='input-group'>
-                    <input type="text" name="search" class='form-control' placeholder="{{ trans('adminlte_lang::message.search') }}"/>
+                    <input type="text" name="search" class='form-control' placeholder="{!! trans('message.search') !!}"/>
                     <div class="input-group-btn">
                         <button type="submit" name="submit" class="btn btn-danger btn-flat"><i class="fa fa-search"></i></button>
                     </div>
@@ -31,4 +24,5 @@
             </form>
         </div>
     </div><!-- /.error-page -->
+</body>
 @endsection
