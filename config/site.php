cda8f5ba77c6385e81562f2e32a990a65ce4ce00
Greg Duckworth <gregory.duckworth@bbc.co.uk>
2016-10-25 15:00:58 +0100

    Ability to turn on/off registration by config

--- config/site.php
+++ config/site.php
@@ -7,4 +7,7 @@
 		'company_name' => 'Duckworth Ltd',
 		'company_url' => 'http://www.google.com',
 
+		// Registration for the site can be on or off
+		'registration' => 'on',
+
 	];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,65 +13,71 @@
                 <h4 class="text-center">{!! trans('message.register') !!}</h4>
             </div>
 
-            <div class="panel-body">
-                {!! Form::open(['url' => 'register', 'class' => 'form-horizontal']) !!}
-                    <div class="form-group">
-                        {!! Form::label('first_name', trans('users.first_name'), ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-8">
-                            {!! Form::text('first_name', null, ['class' => 'form-control']) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('last_name', trans('users.last_name'), ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-8">
-                            {!! Form::text('last_name', null, ['class' => 'form-control']) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group has-feedback">
-                        {!! Form::label('email', trans('message.email'), ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-8">
-                            {!! Form::text('email', null, ['class' => 'form-control']) !!}
-                            <i class="fa fa-envelope form-control-feedback"></i>
-                        </div>
-                    </div>
-
-                    <div class="form-group has-feedback">
-                        {!! Form::label('password', trans('message.password'), ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-8">
-                            {!! Form::password('password', ['class' => 'form-control']) !!}
-                            <i class="fa fa-unlock-alt form-control-feedback"></i>
-                        </div>
-                    </div>
-
-                    <div class="form-group has-feedback">
-                        {!! Form::label('password_confirmation', trans('users.password_confirmation'), ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-8">
-                            {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
-                            <i class="fa fa-lock form-control-feedback"></i>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-offset-4 col-md-8">
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox" name="terms">{!! trans('message.agree_terms') !!}
-                                </label>
+            @if(\Config::get('site.registration') == 'on')
+                <div class="panel-body">
+                    {!! Form::open(['url' => 'register', 'class' => 'form-horizontal']) !!}
+                        <div class="form-group">
+                            {!! Form::label('first_name', trans('users.first_name'), ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-8">
+                                {!! Form::text('first_name', null, ['class' => 'form-control']) !!}
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-offset-4 col-md-8">
-                            {!! Form::submit(trans('message.register'), ['class' => 'btn btn-success']) !!}
+
+                        <div class="form-group">
+                            {!! Form::label('last_name', trans('users.last_name'), ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-8">
+                                {!! Form::text('last_name', null, ['class' => 'form-control']) !!}
+                            </div>
                         </div>
-                    </div>
-                {!! Form::close() !!}
-            </div>
-            <div class="panel-footer">
-                <a href="{!! route('login') !!}" class="text-center">{!! trans('message.membership') !!}</a>
-                <a href="#" class="pull-right" data-toggle="modal" data-target="#termsModal">{!! trans('message.terms') !!}</a>
-            </div>
+
+                        <div class="form-group has-feedback">
+                            {!! Form::label('email', trans('message.email'), ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-8">
+                                {!! Form::text('email', null, ['class' => 'form-control']) !!}
+                                <i class="fa fa-envelope form-control-feedback"></i>
+                            </div>
+                        </div>
+
+                        <div class="form-group has-feedback">
+                            {!! Form::label('password', trans('message.password'), ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-8">
+                                {!! Form::password('password', ['class' => 'form-control']) !!}
+                                <i class="fa fa-unlock-alt form-control-feedback"></i>
+                            </div>
+                        </div>
+
+                        <div class="form-group has-feedback">
+                            {!! Form::label('password_confirmation', trans('users.password_confirmation'), ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-8">
+                                {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+                                <i class="fa fa-lock form-control-feedback"></i>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-offset-4 col-md-8">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="terms">{!! trans('message.agree_terms') !!}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-offset-4 col-md-8">
+                                {!! Form::submit(trans('message.register'), ['class' => 'btn btn-success']) !!}
+                            </div>
+                        </div>
+                    {!! Form::close() !!}
+                </div>
+                <div class="panel-footer">
+                    <a href="{!! route('login') !!}" class="text-center">{!! trans('message.membership') !!}</a>
+                    <a href="#" class="pull-right" data-toggle="modal" data-target="#termsModal">{!! trans('message.terms') !!}</a>
+                </div>
+            @else
+                <div class="panel-body">
+                    <p class="text-center">Sorry currently registration is closed.</p>
+                </div>
+            @endif
         </div>
     </div>
 </div>
