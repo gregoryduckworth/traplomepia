4441ddcc729e422268866079ae80ea37522cc979
Greg Duckworth <gregory.duckworth@bbc.co.uk>
2016-10-31 12:40:33 +0000

    Add additional blades to profile

--- resources/views/users/profile/index.blade.php
+++ resources/views/users/profile/index.blade.php
@@ -28,61 +28,11 @@
     </div>
 </div>
 
-<div class="box">
-    <div class="box-header">
-        <h3 class="box-title">{!! trans('users.change_profile_picture') !!}</h3>
-    </div>
-    <div class="box-body">
-        {!! Form::open(['id' => 'form-file', 'route' => 'api.profile.image', 'redirect' => route('profile.index'), '_method' => 'POST', 'class' => 'col-md-12']) !!}
-            <span id="image-error"></span>
-            <div class="form-group">
-                {!! Form::file('image', ['id' => 'image', 'class' => 'file-loading']) !!}
-            </div>
-        {!! Form::close() !!}
-    </div>
-</div>
-
-<div class="box">
-    <div class="box-header">
-        <h3 class="box-title">{!! trans('users.change_password') !!}</h3>
-    </div>
-    <div class="box-body">
-        {!! Form::open(['id' => 'form', 'route' => 'api.profile.password', 'redirect' => route('profile.index'), '_method' => 'PATCH', 'class' => 'col-md-12']) !!}
-            <div class="form-group">
-                {!! Form::label('old_password', trans('users.old_password')) !!}
-                {!! Form::password('old_password', ['class' => 'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('password', trans('users.new_password')) !!}
-                {!! Form::password('password', ['class' => 'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('password_confirmation', trans('users.password_confirmation')) !!}
-                {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
-            </div>
-            {!! Form::submit(trans('common.submit'), ['class' => 'pull-right btn btn-success']) !!}
-        {!! Form::close() !!}
-    </div>
-</div>
+@include('users.profile.picture')
+@include('users.profile.password')
 
 @endsection
 
 @push('javascript')
-<script>
-$(function() {
-    $("#image").fileinput({ 
-        'showPreview': false,
-        'showRemove': false,
-        'allowedFileTypes': ['image'],
-    }).on('fileerror', function(event, data, msg) {
-        $('#image-error').text(msg);
-    }).on('fileloaded', function(event, data, msg){
-        $('#image-error').text('');
-    });
-});
-</script>
-@include('swal.form')
-@include('swal.form-file')
+
 @endpush
---
+++ resources/views/users/profile/password.blade.php
@@ -0,0 +1,28 @@
+<div class="box">
+    <div class="box-header">
+        <h3 class="box-title">{!! trans('users.change_password') !!}</h3>
+    </div>
+    <div class="box-body">
+        {!! Form::open(['id' => 'form', 'route' => 'api.profile.password', 'redirect' => route('profile.index'), '_method' => 'PATCH', 'class' => 'col-md-12']) !!}
+            <div class="form-group">
+                {!! Form::label('old_password', trans('users.old_password')) !!}
+                {!! Form::password('old_password', ['class' => 'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('password', trans('users.new_password')) !!}
+                {!! Form::password('password', ['class' => 'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('password_confirmation', trans('users.password_confirmation')) !!}
+                {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+            </div>
+            {!! Form::submit(trans('common.submit'), ['class' => 'pull-right btn btn-success']) !!}
+        {!! Form::close() !!}
+    </div>
+</div>
+
+@push('javascript')
+@include('swal.form')
+@endpush
