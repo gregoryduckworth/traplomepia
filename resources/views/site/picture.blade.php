f4000cf5b494c10c7fd92b683b095cf5273209b4
Greg Duckworth <gregory.duckworth@bbc.co.uk>
2016-11-01 13:50:35 +0000

    Update to layout

--- resources/views/site/picture.blade.php
+++ resources/views/site/picture.blade.php
@@ -3,22 +3,35 @@
         <h3 class="box-title">{!! trans('settings.change_site_picture') !!}</h3>
     </div>
     <div class="box-body">
-        {!! Form::open(['id' => 'form-file', 'route' => 'api.settings.image', 'redirect' => route('admin.settings.index'), '_method' => 'POST', 'class' => 'col-md-12']) !!}
-            <span id="image-text"></span>
-            <div class="form-group">
-                {!! Form::file('image', ['id' => 'image', 'class' => 'file-loading']) !!}
-            </div>
-        {!! Form::close() !!}
+        <div class="col-md-4">
+            <h6>Current Picture</h6>
+            <img src="{!! $global_settings['picture'] !!}" class="img-responsive">
+        </div>
+        <div class="col-md-8">
+            <h6>Update Picture</h6>
+            {!! Form::open(['id' => 'form-file', 'route' => 'api.settings.image', 'redirect' => route('admin.settings.index'), '_method' => 'POST', 'class' => 'col-md-12']) !!}
+                <div class="form-group">
+                    {!! Form::file('image', ['id' => 'image', 'class' => 'file-loading']) !!}
+                </div>
+            {!! Form::close() !!}
+        </div>
     </div>
 </div>
 
 @push('javascript')
 <script>
+// TODO: Update it to upload the picture through uploadURL
+// - Watch out for 401 error, which could be due
+// to the api_token or _token
+// 
+// //uploadUrl: '{!! route('api.settings.image') !!}',
+// 
 $(function() {
     $("#image").fileinput({ 
-        //uploadUrl: '{!! route('api.settings.image') !!}',
         showRemove: false,
         allowedFileTypes: ['image'],
+        browseOnZoneClick: true,
+        dropZoneTitle: 'Drag &amp; drop new image here...',
     });
 });
 </script>
