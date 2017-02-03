5fcf91c5566e14149e5e5b4dc15b997218c96cbb
Greg Duckworth <gregoryduckworth@googlemail.com>
2017-02-03 10:32:15 +0000

    Move from bower to node_modules

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const { mix } = require('laravel-mix');
+const folder = 'node_modules/';
 
 /*
  |--------------------------------------------------------------------------
@@ -11,72 +12,39 @@ const { mix } = require('laravel-mix');
  |
  */
 
-// Bootstrap
-mix.copy('bower_components/bootstrap/dist/css/bootstrap.css', 'public/css/libs/bootstrap.css');
-mix.copy('bower_components/bootstrap/dist/js/bootstrap.js', 'public/js/libs/bootstrap.js');
-
 // Glyphicons
-mix.copy('bower_components/bootstrap/dist/fonts', 'public/build/fonts/bootstrap');
-
-// Bootstrap-filestyle
-mix.copy('bower_components/bootstrap-filestyle/src/bootstrap-filestyle.js', 'public/js/libs/bootstrap-filestyle.js');
-
-// Jquery-Slimscroll
-mix.copy('bower_components/jquery-slimscroll/jquery.slimscroll.js', 'public/js/libs/jquery.slimscroll.js');
-
-// Jquery-Slimscroll
-mix.copy('bower_components/fastclick/lib/fastclick.js', 'public/js/libs/fastclick.js');
-
-// AdminLTE
-mix.copy('bower_components/adminlte/dist/css/adminlte.css', 'public/css/libs/admin-lte.css');
-mix.copy('bower_components/adminlte/dist/css/skins/_all-skins.css', 'public/css/libs/admin-lte-skin.css');
-mix.copy('bower_components/adminlte/dist/js/app.js', 'public/js/libs/admin-lte.js');
+mix.copy(folder + 'bootstrap/dist/fonts', 'public/fonts/bootstrap');
 
 // Font Awesome
-mix.copy('bower_components/font-awesome/css/font-awesome.css', 'public/css/libs/font-awesome.css');
-mix.copy('bower_components/font-awesome/fonts', 'public/fonts');
-
-// DataTables
-mix.copy('bower_components/datatables.net-bs/css/dataTables.bootstrap.css', 'public/css/libs/datatables.css');
-mix.copy('bower_components/datatables.net-bs/js/dataTables.bootstrap.js', 'public/js/libs/datatables.bootstrap.js');
-mix.copy('bower_components/datatables.net/js/jquery.dataTables.js', 'public/js/libs/datatables.js');
-
-// SweetAlert
-mix.copy('bower_components/sweetalert/dist/sweetalert.css', 'public/css/libs/sweetalert.css');
-mix.copy('bower_components/sweetalert/dist/sweetalert.min.js', 'public/js/libs/sweetalert.js');
+mix.copy(folder + 'font-awesome/fonts', 'public/fonts');
 
 // Bootstrap Fileinput
-mix.copy('bower_components/bootstrap-fileinput/css/fileinput.css', 'public/css/libs/bootstrap-fileinput.css');
-mix.copy('bower_components/bootstrap-fileinput/js/fileinput.js', 'public/js/libs/bootstrap-fileinput.js');
-mix.copy('bower_components/bootstrap-fileinput/img/loading.gif', 'public/build/img/loading.gif');
-mix.copy('bower_components/bootstrap-fileinput/img/loading-sm.gif', 'public/build/img/loading-sm.gif');
-
-// JQuery
-mix.copy('bower_components/jquery/dist/jquery.js', 'public/js/libs/jquery.js');
+mix.copy(folder + 'bootstrap-fileinput/img/loading.gif', 'public/img/loading.gif');
+mix.copy(folder + 'bootstrap-fileinput/img/loading-sm.gif', 'public/img/loading-sm.gif');
 
 // Merge all CSS files in one file.
 mix.combine([
-    'public/css/libs/admin-lte.css',
-    'public/css/libs/admin-lte-skin.css',
-    'public/css/libs/bootstrap.css',
-    'public/css/libs/datatables.css',
-    'public/css/libs/font-awesome.css',
-    'public/css/libs/sweetalert.css',
-    'public/css/libs/bootstrap-fileinput.css',
-], 'public/css/external.css');
+    folder + 'admin-lte/dist/css/AdminLTE.css',
+    folder + 'admin-lte/dist/css/skins/_all-skins.css',
+    folder + 'bootstrap/dist/css/bootstrap.css',
+    folder + 'datatables.net-bs/css/dataTables.bootstrap.css',
+    folder + 'font-awesome/css/font-awesome.css',
+    folder + 'sweetalert/dist/sweetalert.css',
+    folder + 'bootstrap-fileinput/css/fileinput.css',
+], 'public/css/external.css');//.extract(['vendors']);
 
 // Merge all JS  files in one file.
 mix.combine([
-    'public/js/libs/jquery.js',
-    'public/js/libs/admin-lte.js',
-    'public/js/libs/bootstrap.js',
-    'public/js/libs/bootstrap-filestyle.js',
-    'public/js/libs/datatables.js',
-    'public/js/libs/datatables.bootstrap.js',
-    'public/js/libs/fastclick.js',        
-    'public/js/libs/jquery.slimscroll.js',
-    'public/js/libs/sweetalert.js',
-    'public/js/libs/bootstrap-fileinput.js',
+    folder + 'jquery/dist/jquery.js',
+    folder + 'admin-lte/dist/js/app.js',
+    folder + 'bootstrap/dist/js/bootstrap.js',
+    folder + 'bootstrap-fileinput/js/fileinput.js',
+    folder + 'datatables.net-bs/js/dataTables.bootstrap.js',
+    folder + 'datatables/media/js/jquery.dataTables.js',
+    folder + 'fastclick/lib/fastclick.js',
+    folder + 'sweetalert/dist/sweetalert.min.js',
+    folder + 'jquery-slimscroll/jquery.slimscroll.js',
+    folder + 'bootstrap-fileinput/js/fileinput.js',
 ], 'public/js/external.js');
 
 mix.js('resources/assets/js/app.js', 'public/js')
